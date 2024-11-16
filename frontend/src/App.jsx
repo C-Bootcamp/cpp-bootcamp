eefde8911978034cebdc3066eccf9dbc847809ea
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-16 16:22:22 +0530

    2nd page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FirstPage from "./Pages/FirstPage";
+import SecondPage from "./Pages/SecondPage";
 
 const App = () => {
   return (
     <div className="w-full min-h-screen">
       <FirstPage />
+      <SecondPage />
     </div>
   );
 };
---
+++ frontend/src/Pages/SecondPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SecondPage = () => {
+  return (
+    <div className='w-full min-h-screen bg-red-400 text-9xl font-thin text-center'>
+      add new pages
+    </div>
+  );
+}
+
+export default SecondPage;
