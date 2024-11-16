c8149df00dddae044a6ff7d94de0f9751dc00bc0
siddharthswami23 <siddharth.swami@walchandsangli.ac.in>
2024-11-16 16:11:11 +0530

    done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,33 +9,37 @@ const Navbar = () => {
       <div className="max-w-9xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-            <img src="https://i.redd.it/3d-rendered-the-cyberpunk-logo-using-blender-v0-khopj82wqee91.png?width=1920&format=png&auto=webp&s=f42dbd8f5c04aedb9ba9291c1ba4d679029cc1f3" alt="Logo" />
+            <img
+              className="z-[3] h-32 mix-blend-screen filter saturate-150 contrast-125"
+              src="https://i.redd.it/3d-rendered-the-cyberpunk-logo-using-blender-v0-khopj82wqee91.png?width=1920&format=png&auto=webp&s=f42dbd8f5c04aedb9ba9291c1ba4d679029cc1f3"
+              alt="Logo"
+            />
           </div>
 
           <div className="md:hidden">
             <button
               onClick={() => setIsOpen(!isOpen)}
-              className="text-white focus:outline-none"
+              className="text-white focus:outline-none z-[20]"
             >
               {isOpen ? (
-                <FaTimes className="h-6 w-6" />
+                <FaTimes className="h-6 w-6 z-[20]" />
               ) : (
-                <FaBars className="h-6 w-6" />
+                <FaBars className="h-6 w-6 z-[20]" />
               )}
             </button>
           </div>
 
           <div className="hidden md:flex space-x-4 text-2xl text-white">
-            <a href="#home" className="z-[10] hover:text-blue-500">
+            <a href="#home" className="z-[12] hover:text-blue-500">
               Home
             </a>
-            <a href="#about" className="z-[10] hover:text-blue-500">
+            <a href="#about" className="z-[12] hover:text-blue-500">
               About
             </a>
-            <a href="#services" className="z-[10] hover:text-blue-500">
+            <a href="#services" className="z-[12] hover:text-blue-500">
               Services
             </a>
-            <a href="#contact" className="z-[10] hover:text-blue-500">
+            <a href="#contact" className="z-[12] hover:text-blue-500">
               Contact
             </a>
           </div>
--- frontend/src/components/SceneCanvas.jsx
+++ frontend/src/components/SceneCanvas.jsx
@@ -6,25 +6,19 @@ import {
 } from "@react-three/drei";
 import DamagedHelmet from "./DamagedHelmet";
 import CanvasLoader from "./CanvasLoader";
-import { Suspense, useRef } from "react";
+import { Suspense, forwardRef, useRef } from "react";
 import { useMediaQuery } from "react-responsive";
-import gsap from "gsap";
 
-const SceneCanvas = () => {
+const SceneCanvas = forwardRef((props, ref) => {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-  
-  
 
   return (
-    <div
-      className="w-full h-full absolute inset-0 z-[2]"
-      onMouseMove={handleMouseMove}
-    >
+    <div className="w-full h-full absolute inset-0 z-[2]">
       <Canvas shadows>
         <Suspense fallback={<CanvasLoader />}>
           <PerspectiveCamera makeDefault position={[0, 0, 30]} />
           <DamagedHelmet
-            ref={modelRef}
+            ref={ref}
             scale={isMobile ? 10 : 13}
             position={[0, 2, 0]}
           />
@@ -34,10 +28,11 @@ const SceneCanvas = () => {
             files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/moonless_golf_4k.hdr"
             background={false}
           />
+          <OrbitControls />
         </Suspense>
       </Canvas>
     </div>
   );
-};
+});
 
 export default SceneCanvas;
